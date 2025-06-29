fdad4f27946dd4aa5472daf7c5ccabd882cd2706
KG7777 <gp3chita@yandex.ru>
2025-06-29 20:03:26 +0900

    Исправления по архиву. Вставка архива и удаление из user

--- 4.6.py
+++ 4.6.py
@@ -8,7 +8,8 @@ client = MongoClient("mongodb://localhost:27017/")
 db = client["my_database"]
 collection = db["user_events"]
 
-#db.create_collection("archived_users")
+#db["archived_users"].drop()
+#db.create_collection("archived_users") 
 
 # Текущая дата и дата 30 дней назад
 current_date = datetime.now()
@@ -16,42 +17,45 @@ start_date = current_date - timedelta(days=30)
 active_date = current_date - timedelta(days=14)
 arc_users_count = 0
 
+#коллекция для архива
+collection_archived = db["archived_users"]
+
 # Получить с фильтром
 results = collection.find({"user_info": {"$exists":True}})
 start_date = datetime.now() - timedelta(30)
 my_list_ids = [] 
-#print(date.today())
+
+#находим пользователей необходимых
 for doc in results:
-    #print(doc["user_info"]["registration_date"] )
-    #print(doc["event_time"])
     if doc["user_info"]["registration_date"] < start_date and doc["event_time"] < active_date :   
         arc_users_count += 1
         my_list_ids.append(doc["user_id"]) 
+        #копируем нужный элемент коллекции
+        doc_to_transfer = doc.copy()
+
+        #вставляем во вторую коллекцию
+        collection_archived.insert_one(doc_to_transfer)
+        #удаляем из первой коллекции
+        result_delete = collection.delete_one({"user_id": doc["user_id"]})
+        print(f"Удален: {doc["user_id"]}")
         
-# Преобразуем date в datetime
-today_datetime = datetime.combine(date.today(), datetime.min.time())
-#print(date.today().isoformat())
-
-#print(arc_users_count)
-#print(my_list_ids)
-
-archived_user = {
+#коллекция для отчета
+archived_data_file = {
     "date": date.today().isoformat(),
     "archived_users_count": arc_users_count,
     "archived_users_ids": my_list_ids
 }
 
-collection_archived = db["archived_users"]
-result_delete = collection_archived.delete_many({"date": archived_user["date"]})
-print(f"Удалено кол-во: {result_delete.deleted_count}")
-
-result = db.archived_users.insert_one(archived_user)
-print(f"Запись добавлена с датой: {archived_user['date']}")  
-
 print("\nВсе документы в коллекции archived_users:")
 for doc in db.archived_users.find():
     print(doc)
 
+print("\nВсе документы в коллекции user_events:")
+for doc in db.user_events.find():
+    print(doc)
+
 # Сохраняем в файл
 with open(f"{date.today()}.json", "w") as f:
-     json.dump(archived_user, f, indent=2, default=str)
+     json.dump(archived_data_file, f, indent=2, default=str)
+
+client.close
